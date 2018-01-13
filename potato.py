f82df953082595e85e094c107d3416ade4130448
Nicole Martindale <nmartind@ucsd.edu>
2018-01-13 15:00:52 -0800

    Update potato.py

--- potato.py
+++ potato.py
@@ -3,6 +3,6 @@ import snowboydecoder
 def detected_callback():
     print "hotword detected"
 
-detector = snowboydecoder.HotwordDetector("resources/snowboy.umdl", sensitivity = 0.5, audio_gain = 1)
+detector = snowboydecoder.HotwordDetector("Hey_Potato.pmdl", sensitivity = 0.6, audio_gain = 1)
 
 detector.start(detected_callback)
