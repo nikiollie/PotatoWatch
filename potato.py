d002c3e9145b9c9bf9f789722d0e0d2de7879536
Nicole Martindale <nmartind@ucsd.edu>
2018-01-13 15:30:59 -0800

    Update potato.py

--- potato.py
+++ potato.py
@@ -1,7 +1,19 @@
-import snowboydecoder
+import snowboydecoder #hotword
+import mraa 
+import time 
+
+#initialize gpio 23
+gpio_1 = mraa.Gpio(23)
+
+#set gpio 23 to output
+gpio_1.dir(mraa.DIR_OUT)
 
 def detected_callback():
     print "hotword detected"
+    #sends signal to arduino to make motor move for 2 seconds
+    gpio_1.write(1)
+    time.sleep(2)
+    gpio_1.write(0)
 
 detector = snowboydecoder.HotwordDetector("Hey_Potato.pmdl", sensitivity = 0.6, audio_gain = 1)
 
